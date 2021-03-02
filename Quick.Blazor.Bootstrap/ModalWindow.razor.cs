e9b5c518293ffcf5bdf39f6dcc2debb2989f134b
张鹏 <scbeta@qq.com>
2021-03-02 18:13:00 +0800

    增加组件[ModalWindow]。

---
+++ Quick.Blazor.Bootstrap/ModalWindow.razor.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Components;
+using Quick.Blazor.Bootstrap.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quick.Blazor.Bootstrap
+{
+    public partial class ModalWindow
+    {
+        private string Title { get; set; }
+        private RenderFragment Content { get; set; }
+        private Action OkCallback { get; set; }
+        private Action CancelCallback { get; set; }
+        private bool Visiable { get; set; }
+
+        public void Show(string title, Type componentType, Dictionary<string, object> parameterDict = null)
+        {
+            Title = title;
+            Content = BlazorUtils.GetRenderFragment(componentType, parameterDict);
+            Visiable = true;
+            InvokeAsync(StateHasChanged);
+        }
+
+        public void Close()
+        {
+            Title = null;
+            Content = null;
+            Visiable = false;
+        }
+    }
+}
---
+++ Quick.Blazor.Bootstrap/Utils/BlazorUtils.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quick.Blazor.Bootstrap.Utils
+{
+    public class BlazorUtils
+    {
+        public static RenderFragment GetRenderFragment(Type componentType, Dictionary<string, object> parameterDict)
+        {
+            return builder =>
+            {
+                builder.OpenComponent(0, componentType);
+                if (parameterDict != null)
+                {
+                    var currentIndex = 1;
+                    foreach (var key in parameterDict.Keys)
+                    {
+                        var pi = componentType.GetProperty(key);
+                        if (pi == null)
+                            continue;
+                        builder.AddAttribute(currentIndex, key, parameterDict[key]);
+                        currentIndex++;
+                    }
+                }
+                builder.CloseComponent();
+            };
+        }
+    }
+}
