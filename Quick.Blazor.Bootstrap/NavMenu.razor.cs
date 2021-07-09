b657c59011e72356cf32b2b01851297ee0f3e26e
张鹏 <scbeta@qq.com>
2021-07-09 10:53:36 +0800

    Add component 'NavMenu'

---
+++ Quick.Blazor.Bootstrap/NavMenu.razor.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quick.Blazor.Bootstrap
+{
+    public partial class NavMenu
+    {
+        [Parameter]
+        public EventCallback OnTitleClick { get; set; }
+        [Parameter]
+        public string Title { get; set; }
+        [Parameter]
+        public string ActiveKey { get; set; }
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+        [Parameter]
+        public EventCallback<string> OnTabClick { get; set; }
+        [Parameter]
+        public EventCallback<string> ActiveKeyChanged { get; set; }
+        [Parameter]
+        public EventCallback<string> OnChange { get; set; }
+        [Parameter]
+        public string NavTabsExtraClass { get; set; }
+        [Parameter]
+        public string TabContentExtraClass { get; set; }
+
+        internal List<NavMenuItem> _panes = new List<NavMenuItem>();
+
+        private string _activeKey = null;
+        private NavMenuItem _activePane = null;
+
+        private bool collapseNavMenu = true;
+
+        private string NavMenuCssClass => collapseNavMenu ? "collapse" : null;
+
+        private void ToggleNavMenu()
+        {
+            collapseNavMenu = !collapseNavMenu;
+        }
+
+        internal void AddTabPane(NavMenuItem tabPane)
+        {
+            if (string.IsNullOrEmpty(tabPane.Key))
+            {
+                throw new ArgumentNullException(nameof(tabPane), "Key is null");
+            }
+
+            if (_panes.Select(p => p.Key).Contains(tabPane.Key))
+            {
+                throw new ArgumentException("An NavMenuItem with the same key already exists");
+            }
+            _panes.Add(tabPane);
+        }
+
+        protected override void OnAfterRender(bool firstRender)
+        {
+            base.OnAfterRender(firstRender);
+            if (!firstRender || _panes.Count == 0)
+                return;
+
+            NavMenuItem panel = null;
+            if (!string.IsNullOrEmpty(ActiveKey))
+                panel = _panes.FirstOrDefault(t => t.Key == ActiveKey);
+            if (panel == null)
+                panel = _panes.First();
+            ActivatePane(panel);
+        }
+
+        private void ActivatePane(NavMenuItem tabPane)
+        {
+            if (!tabPane.Disabled && _panes.Contains(tabPane))
+            {
+                if (_activePane != null)
+                {
+                    _activePane.IsActive = false;
+                }
+                tabPane.IsActive = true;
+                _activePane = tabPane;
+                if (_activeKey != _activePane.Key)
+                {
+                    if (!string.IsNullOrEmpty(_activeKey))
+                    {
+                        if (ActiveKeyChanged.HasDelegate)
+                        {
+                            ActiveKeyChanged.InvokeAsync(_activePane.Key);
+                        }
+
+                        if (OnChange.HasDelegate)
+                        {
+                            OnChange.InvokeAsync(_activePane.Key);
+                        }
+                    }
+                    _activeKey = _activePane.Key;
+                }
+                StateHasChanged();
+            }
+        }
+
+        internal void HandleTabClick(NavMenuItem tabPane)
+        {
+            if (tabPane.IsActive)
+                return;
+
+            if (OnTabClick.HasDelegate)
+            {
+                OnTabClick.InvokeAsync(tabPane.Key);
+            }
+
+            ActivatePane(tabPane);
+        }
+    }
+}
---
+++ Quick.Blazor.Bootstrap/NavMenuItem.razor.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quick.Blazor.Bootstrap
+{
+    public partial class NavMenuItem : ComponentBase
+    {
+        [Parameter]
+        public EventCallback OnClick { get; set; }
+
+        [Parameter]
+        public string Key { get; set; }
+        [Parameter]
+        public RenderFragment Tab { get; set; }
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+        [Parameter]
+        public bool Disabled { get; set; }
+        [Parameter]
+        public bool Closable { get; set; }
+
+        private bool _isActive;
+        internal bool IsActive
+        {
+            get => _isActive;
+            set
+            {
+                if (_isActive != value)
+                {
+                    _isActive = value;
+                    OnClick.InvokeAsync();
+                }
+            }
+        }
+        [CascadingParameter(Name = "IsEmpty")]
+        private bool IsEmpty { get; set; }
+        [CascadingParameter(Name = "Pane")]
+        private NavMenuItem Pane { get; set; }
+        [CascadingParameter]
+        private NavMenu Parent { get; set; }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+
+            if (IsEmpty)
+            {
+                Parent?.AddTabPane(this);
+            }
+        }
+    }
+}
