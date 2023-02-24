92de01cba84dacac540e85babab63eee5f7e115f
张鹏 <scbeta@qq.com>
2023-02-24 17:18:58 +0800

    Add class: SettingComponentBase

---
+++ Quick.Blazor.Bootstrap/SettingComponentBase.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Components;
+using Quick.Blazor.Bootstrap.Utils;
+using System;
+
+namespace Quick.Blazor.Bootstrap
+{
+    public abstract class SettingComponentBase<T> : ComponentBase
+                where T : new()
+    {
+        public static string ConfigFileFolder { get; private set; }
+        public static T GlobalConfig { get; private set; }
+
+        public T Config { get; private set; }
+
+        public static void Init(string configFileFolder)
+        {
+            ConfigFileFolder = configFileFolder;
+            GlobalConfig = ConfigFileUtils.Load<T>(folder: ConfigFileFolder);
+            if (GlobalConfig == null)
+                GlobalConfig = new T();
+        }
+
+        protected override void OnInitialized()
+        {
+            Config = ConfigFileUtils.Load<T>(folder: ConfigFileFolder);
+            if (Config == null)
+                Config = new T();
+        }
+
+        protected void save()
+        {
+            ConfigFileUtils.Save(Config, folder: ConfigFileFolder);
+            GlobalConfig = Config;
+        }
+    }
+}
---
+++ Quick.Blazor.Bootstrap/Utils/ConfigFileUtils.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quick.Blazor.Bootstrap.Utils
+{
+    internal class ConfigFileUtils
+    {
+        private static string getTypeFilePath(Type type, string fileSuffix, string folder)
+        {
+            var fileName = type.FullName;
+            if (!string.IsNullOrEmpty(fileSuffix))
+                fileName += "." + fileSuffix;
+            fileName += ".json";
+            if (string.IsNullOrEmpty(folder))
+                return fileName;
+            return Path.Combine(folder, fileName);
+        }
+
+        public static string GetTypeFilePath<T>(string fileSuffix, string folder = null)
+        {
+            return getTypeFilePath(typeof(T), fileSuffix, folder);
+        }
+
+        public static T Load<T>(string fileSuffix = null, string folder = null)
+        {
+            try
+            {
+                var file = GetTypeFilePath<T>(fileSuffix, folder);
+                if (!File.Exists(file))
+                    return default(T);
+                var content = File.ReadAllText(file);
+                return JsonConvert.DeserializeObject<T>(content);
+            }
+            catch
+            {
+                return default(T);
+            }
+        }
+
+        public static void Save(object configObj, string fileSuffix = null, string folder = null)
+        {
+            if (configObj == null)
+                return;
+
+            var file = getTypeFilePath(configObj.GetType(), fileSuffix, folder);
+            if (File.Exists(file))
+                File.Delete(file);
+            var content = JsonConvert.SerializeObject(configObj);
+            File.WriteAllText(file, content, Encoding.UTF8);
+        }
+    }
+}
