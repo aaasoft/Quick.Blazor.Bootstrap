407e9240a8f76b452a637d1145f0d4d546ab639f
张鹏 <scbeta@qq.com>
2025-03-03 10:15:58 +0800

    ModalWindow增加将自身实例传递到内容的参数中的功能。增加更新标题和更新内容的功能。

--- Quick.Blazor.Bootstrap/ModalWindow.razor.cs
+++ Quick.Blazor.Bootstrap/ModalWindow.razor.cs
@@ -2,64 +2,75 @@
 using Quick.Blazor.Bootstrap.Utils;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Quick.Blazor.Bootstrap
+namespace Quick.Blazor.Bootstrap;
+
+public partial class ModalWindow : ComponentBase
 {
-    public partial class ModalWindow : ComponentBase
+    [Parameter]
+    public bool DialogCentered { get; set; } = true;
+    [Parameter]
+    public bool DialogScrollable { get; set; } = true;
+    [Parameter]
+    public bool DialogSizeSmall { get; set; }
+    [Parameter]
+    public bool DialogSizeLarge { get; set; }
+    [Parameter]
+    public bool DialogSizeExtraLarge { get; set; }
+    [Parameter]
+    public string ModalHeaderCls { get; set; }
+    [Parameter]
+    public string ModalBodyCls { get; set; }
+    [Parameter]
+    public string ModalFooterCls { get; set; }
+    private string Title { get; set; }
+    private RenderFragment Content { get; set; }
+    private bool Visiable { get; set; }
+
+    public void Show<T>(string title, Dictionary<string, object> parameterDict = null)
     {
-        [Parameter]
-        public bool DialogCentered { get; set; } = true;
-        [Parameter]
-        public bool DialogScrollable { get; set; } = true;
-        [Parameter]
-        public bool DialogSizeSmall { get; set; }
-        [Parameter]
-        public bool DialogSizeLarge { get; set; }
-        [Parameter]
-        public bool DialogSizeExtraLarge { get; set; }
-        [Parameter]
-        public string ModalHeaderCls { get; set; }
-        [Parameter]
-        public string ModalBodyCls { get; set; }
-        [Parameter]
-        public string ModalFooterCls { get; set; }
-        private string Title { get; set; }
-        private RenderFragment Content { get; set; }
-        private bool Visiable { get; set; }
+        Show(title, typeof(T), parameterDict);
+    }
 
-        public void Show<T>(string title, Dictionary<string, object> parameterDict = null)
-        {
-            Show(title, typeof(T), parameterDict);
-        }
+    public void Show(string title, Type componentType, Dictionary<string, object> parameterDict = null)
+    {
+        if (parameterDict == null)
+            parameterDict = new();
+        parameterDict[nameof(ModalWindow)] = this;
+        Show(title, BlazorUtils.GetRenderFragment(componentType, parameterDict));
+        InvokeAsync(StateHasChanged);
+    }
 
-        public void Show(string title, Type componentType, Dictionary<string, object> parameterDict = null)
-        {
-            Show(title, BlazorUtils.GetRenderFragment(componentType, parameterDict));
-            InvokeAsync(StateHasChanged);
-        }
+    public void Show(string title, RenderFragment content)
+    {
+        Title = title;
+        Content = content;
+        Visiable = true;
+        InvokeAsync(StateHasChanged);
+    }
 
-        public void Show(string title, RenderFragment content)
-        {
-            Title = title;
-            Content = content;
-            Visiable = true;
-            InvokeAsync(StateHasChanged);
-        }
+    public void UpdateTitle(string title)
+    {
+        Title = title;
+        InvokeAsync(StateHasChanged);
+    }
 
-        public void Close()
-        {
-            Title = null;
-            Content = null;
-            Visiable = false;
-            InvokeAsync(StateHasChanged);
-        }
+    public void UpdateContent(RenderFragment content)
+    {
+        Content = content;
+        InvokeAsync(StateHasChanged);
+    }
 
-        public void SwitchDialogSizeExtraLarge()
-        {
-            DialogSizeExtraLarge = !DialogSizeExtraLarge;
-        }
+    public void Close()
+    {
+        Title = null;
+        Content = null;
+        Visiable = false;
+        InvokeAsync(StateHasChanged);
+    }
+
+    public void SwitchDialogSizeExtraLarge()
+    {
+        DialogSizeExtraLarge = !DialogSizeExtraLarge;
     }
 }
