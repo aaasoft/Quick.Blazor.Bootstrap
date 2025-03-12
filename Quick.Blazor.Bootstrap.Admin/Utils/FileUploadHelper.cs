95230e6a2a085e8c2a4275f7f75491d96156cca9
张鹏 <scbeta@qq.com>
2025-03-12 20:55:34 +0800

    增加文件上传辅助类：FileUploadHelper

---
+++ Quick.Blazor.Bootstrap.Admin/Utils/FileUploadHelper.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace Quick.Blazor.Bootstrap.Admin.Utils;
+
+public static class FileUploadHelper
+{
+    private readonly static UnitStringConverting storageUSC = UnitStringConverting.StorageUnitStringConverting;
+    
+    public static async Task<string> UploadAsync(
+        InputFileChangeEventArgs e,
+        Action<string> fileInfoHandler,
+        Action<int?, string> progressUpdated,
+        CancellationTokenSource cts)
+    {
+        var cancellationToken = cts.Token;
+        string tmpFile = null;
+        try
+        {
+            var file = e.GetMultipleFiles().FirstOrDefault();
+            using (var commonTransferContext = new CommonTransferContext(progress =>
+            {
+                progressUpdated?.Invoke(progress.Percent, progress.Message);
+            }, file.Size))
+            {
+                var currentFileInfoStr = $"{file.Name} ({storageUSC.GetString(file.Size, 0, true)}B)";
+                fileInfoHandler?.Invoke(currentFileInfoStr);
+                tmpFile = Path.GetTempFileName();
+
+                using (Stream stream = file.OpenReadStream(file.Size, cancellationToken))
+                using (var fileStream = File.OpenWrite(tmpFile))
+                    await commonTransferContext.TransferAsync(stream, fileStream, cancellationToken, file.Size);
+
+                //返回上传的文件
+                return tmpFile;
+            }
+        }
+        catch
+        {
+            if (tmpFile != null && File.Exists(tmpFile))
+                try { File.Delete(tmpFile); } catch { }
+            throw;
+        }
+    }
+}
